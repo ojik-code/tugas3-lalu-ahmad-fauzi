1c99adfbf768c329fdeb2c3f53cd2aa3282b6681
ojik-code <laluahmadfauzi542@gmail.com>
2021-12-08 15:17:16 +0800

    tugas3-lalu-ahmad-fauzi

---
+++ README.md
@@ -0,0 +1,7 @@
+## ewanPedia.com | ListView
+
+Nama  : Lalu Ahmad Fauzi
+NIM   : 200602075
+Kelas : TI.3.C
+
+
---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,92 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Burung;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing("Angora", "Turki",
+                "Aslinya berasal dari Ankara, Turki dengan ciri khas berbulu panjang dan lembut", R.drawable.cat_angora));
+        kucings.add(new Kucing("Bengal", "Inggris",
+                "Merupakan hasil persilangan antara Kucing Leopard Asia dengan kucing domestik Inggris", R.drawable.cat_bengal));
+        kucings.add(new Kucing("Birmani", "Birma/Myanmar",
+                "Konon awalnya merupakan ras kucing yand biasa menemani para biksu yang menetap pada kuil di Birma", R.drawable.cat_birman));
+        kucings.add(new Kucing("Persia", "Iran",
+                "Kucing berbulu panjang dengan wajah bundar dan hidung pesek. Awalnya berasal dari Iran kemudian diimpor ke Italia", R.drawable.cat_persia));
+        kucings.add(new Kucing("Siam", "Thailand",
+                "Kucing hasil turunan dari Wichian Maat (Kucing Thailand) dengan ciri khas berbadan panjang dan langsing, wajah lonjong, telinga lebar, dan mata kebiruan", R.drawable.cat_siam));
+        kucings.add(new Kucing("Siberia", "Rusia",
+                "Kucing domestik Rusia dengan bulu lebat dan badan besar tapi sangat lincah dan terkenal dengan kemampuan melompatnya ", R.drawable.cat_siberian));
+        return kucings;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing("Bulldog", "Inggris",
+                "Anjing populer yang dikenal dengan badan yang gemuk berotot, wajah seperti kain kusut, dan hidung yang pesek", R.drawable.dog_bulldog));
+        anjings.add(new Anjing("Husky", "Alaska,Siberia,Finlandia (daerah bersalju) ",
+                "Anjing jenis khusus yang awalnya digunakan untuk menarik kereta salju", R.drawable.dog_husky));
+        anjings.add(new Anjing("Kintamani", "Indonesia",
+                "Ras anjing asli pulau Bali ", R.drawable.dog_kintamani));
+        anjings.add(new Anjing("Samoyed", "Rusia",
+                "Anjing yang berasal dari Siberia yang awalnya biasa digunakan untuk menjaga ternak oleh suku Samoyed", R.drawable.dog_samoyed));
+        anjings.add(new Anjing("Shepherd", "Jerman",
+                "Anjing pintar dan kuat, sekarang banyak digunakan sebagai anjing penjaga, pengawal, dan anjing polisi (K9)", R.drawable.dog_shepherd));
+        anjings.add(new Anjing("Shiba", "Jepang",
+                "Anjing dari daerah Shiba ini gesit dan lincah sehingga awalnya banyak digunakan untuk berburu ", R.drawable.dog_shiba));
+        return anjings;
+    }
+    private static List<Burung> initDataBurung(Context ctx) {
+        List<Burung> burungs = new ArrayList<>();
+        burungs.add(new Burung("Cendrawasih", "Indonesia Bagian Timur",
+                "Burung cendrawasih (Paradisaeidae) dijuluki sebagai  burung surga karena keelokannya yang terkenal di seluruh dunia.", R.drawable.bird_cendrawasih));
+        burungs.add(new Burung("Flaminggo", "Amerika",
+                "Flamingo adalah spesies burung berkaki panjang yang hidup berkelompok.", R.drawable.bird_flamingo));
+        burungs.add(new Burung("Golden Pheasant", "Tiongkok",
+                "Golden Pheasant adalah burung spesies Neognata faisánido galleriforme yaitu salah satu dari dua spesies dari genus Chrysolophus", R.drawable.bird_golden_pheasant));
+        burungs.add(new Burung("Macau", "Amerika Selatan",
+                "Burung macaw merupakan spesies burung asli dari benua amerika tepatnya di amerika selatan di hutan amazon terdapat ribuan spesies burung macaw", R.drawable.bird_macau));
+        burungs.add(new Burung("Merak", "India",
+                "Burung merak adalah satwa asli dari India. Ada beberapa spesies burungmerak seperti merak hijau, merak putih, merak merah dan merak biru.", R.drawable.bird_merak));
+        burungs.add(new Burung("Tucan", "Brazil",
+                "Tucan adalah burung yang dicirikan oleh paruh bear, warna yang indah dan terang. Paruh ini menonjol di badannya", R.drawable.bird_tucan));
+        return burungs;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(initDataBurung(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
